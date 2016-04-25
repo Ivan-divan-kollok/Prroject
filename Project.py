d06c248118f40079d44c5f662eeb63a4b87f9b80
Ivan-divan-kollok <ivan78_96@mail.ru>
2016-04-25 22:50:22 +0300

    Added files via upload
    Попытка сделать горизонтальный блок, по которому должна бы растекаться вода, но не оч(

--- Project.py
+++ Project.py
@@ -33,11 +33,15 @@ screen = pygame.display.set_mode(size)
 
 
 v = vector([0, 0])
-r = vector([random.random()*100, 0])
+r = vector([100+random.random()*100, 0])
 
 prev_t = pygame.time.get_ticks()
 ar = pygame.PixelArray(screen)
-i=0
+
+i=1
+rad=6
+k=0.4
+
 
 while True:
     delta = clock.tick(50) / 1000.0
@@ -49,48 +53,60 @@ while True:
     r.x[1] += v.x[1] * delta-0.5*g*delta*delta
     v.x[1] +=0.5*g*delta
     i+=1
+    j=1
+
+
+    while  (r.x[1]>200)and(r.x[1]<204)and(r.x[0]>100)and(r.x[0]<450):
+        v.x[1] = 50
+        v.x[1] = 0
+        r.x[0] +=v.x[0] * delta
+
+
+
 
 
 
 
     if i < 117:
-        if r.x[0] < 20:
+        if r.x[0] < rad:
             if v.x[0] < 0:
                 v.x[0] = -v.x[0]
-                r.x[0] = 20
-        if r.x[1] < 20:
+                r.x[0] = rad
+        if r.x[1] < rad:
             if v.x[1] < 0:
                 v.x[1] = -v.x[1]
-                r.x[1] = -20
-        if r.x[0] > 580:
+                r.x[1] = -rad
+        if r.x[0] > 600-rad:
             if v.x[0] > 0:
                 v.x[0] = -v.x[0]
-                r.x[0] = 580
+                r.x[0] = 600-rad
 
-        if r.x[1] > 580:
+        if r.x[1] > 600-rad:
             if v.x[1] > 0:
-                v.x[1] = -0.5*v.x[1]
-                r.x[1] = 580
+                v.x[1] = -k*v.x[1]
+                r.x[1] = 600-rad
 
     if i >= 117:
-        if r.x[0] < 20:
+        if r.x[0] < rad:
             if v.x[0] < 0:
                 v.x[0] = -v.x[0]
-                r.x[0] = 20
-        if r.x[1] < 20:
+                r.x[0] = rad
+        if r.x[1] < rad:
             if v.x[1] < 0:
                 v.x[1] = -v.x[1]
-                r.x[1] = -20
-        if r.x[0] > 580:
+                r.x[1] = -rad
+        if r.x[0] > 600-rad:
             if v.x[0] > 0:
                 v.x[0] = -v.x[0]
-                r.x[0] = 580
+                r.x[0] = 600-rad
 
-        if r.x[1] > 580:
+        if r.x[1] > 600-rad:
             if v.x[1] > 0:
-                v.x[1] = -0.5*v.x[1]
-                r.x[1] = 580
-        v.x[0] = 10000*(random.random()*10*(-1)**i)/(i)
+                v.x[1] = -k*v.x[1]
+                r.x[1] = 600-rad
+        #v.x[0] = 5000*(random.random()*10*(-1)**i)/(i)
+
+
 
 
 
@@ -99,5 +115,5 @@ while True:
 
     screen.fill((0, 0, 0))
     col = 255
-    pygame.draw.circle(screen, (col/9, col, col/9), (int(r.x[0]), int(r.x[1])), 20)
+    pygame.draw.circle(screen, (col/99, col, col), (int(r.x[0]), int(r.x[1])), rad)
     pygame.display.flip()
