904689aebd4cca8c4c84f37dff5e5c3dd5a0bbf9
Ivan-divan-kollok <ivan78_96@mail.ru>
2016-05-02 22:45:29 +0300

    There will be actions

---
+++ Class_Game.py
@@ -0,0 +1,69 @@
+class Game:
+    def tick(self):
+        """Return time in seconds since previous call
+        and limit speed of the game to 50 fps"""
+        self.delta = self.clock.tick(50) / 1000.0
+
+    def __init__(self):
+        """Constructor of the Game"""
+        self._running = True
+        self.size = self.width, self.height = 640, 400
+        # create main display - 640x400 window
+        # try to use hardware acceleration
+        self.screen = pygame.display.set_mode(self.size, pygame.HWSURFACE)
+        # set window caption
+        pygame.display.set_caption('Game')
+        # get object to help track time
+        self.clock = pygame.time.Clock()
+        # set default tool
+        self.tool = 'run'
+        self.ball = Ball()
+        self.block = Block()
+        self.ar = pygame.PixelArray(self.screen)
+
+    def event_handler(self, event):
+        """Handling one pygame event"""
+        if event.type == pygame.QUIT:
+            # close window event
+            self.exit()
+        elif event.type == pygame.KEYDOWN:
+            # keyboard event on press ESC
+            if event.key == pygame.K_ESCAPE:
+                self.exit()
+
+    def move(self):
+        """Here game objects update their positions"""
+        self.tick()
+        self.pressed = pygame.key.get_pressed()
+
+        self.player.update(self)
+
+    def render(self):
+        """Render the scene"""
+        self.screen.fill((0, 0, 0))
+        self.player.render(self)
+        self.ar[int(self.player.x/10.0),int(self.player.y/10.0)] = (200,200,200)
+
+        for b in self.blocks:
+            b.render()
+
+
+        pygame.display.flip()
+
+    def exit(self):
+        """Exit the game"""
+        self._running = False
+
+    def cleanup(self):
+        """Cleanup the Game"""
+        pygame.quit()
+
+    def execute(self):
+        """Execution loop of the game"""
+        while(self._running):
+            # get all pygame events from queue
+            for event in pygame.event.get():
+                self.event_handler(event)
+            self.move()
+            self.render()
+        self.cleanup()
